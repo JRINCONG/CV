d51ea37aa29a048ba35f8b21ae53b27a1ffd5be7
Jairo Rincon <rincon303@gmail.com>
2024-09-09 20:26:31 -0500

    ruta api

--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -4,17 +4,17 @@ import { useState } from "react";
 export const useFetch = ()=>{
 const [Envio, setEnvio]=useState()
 
-const ruta ='https://send-yxvp.onrender.com/contact'
+const ruta ='https://send-yxvp.onrender.com/api/v1/contact'
 
  const getEnviar = (data)=>{
-    
+    console.log(data)
     axios.post(ruta,data)
     .then(resp => setEnvio(resp.data))
     .catch(error => console.log(error))
 
 
  }
-
+console.log(Envio)
 return [Envio, getEnviar]
     
 }
