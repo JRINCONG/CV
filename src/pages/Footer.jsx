41494215f203bc15a037097e45bcda877e72a6c7
Jairo Rincon <rincon303@gmail.com>
2024-09-11 20:50:20 -0500

    hover

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,10 +4,10 @@ export const Footer = () => {
   return (
     <>
     <div className='Container_footer'>    
-      <hr className='HR'/>
+      
       <p>Ya te lo he ordenado: ¡Sé fuerte y valiente! ¡No tengas miedo ni te desanimes! Porque el Señor tu Dios te acompañará dondequiera que vayas.
       (Josué 1:9)</p>
-      <h4><b>Cartagena- Colombia</b></h4>
+      <h4>Cartagena- Colombia</h4>
     </div>
     </>
   )
